1c281c92e5084f793bd08043c3ba6096353166bb
FSEVERI\ceretta2991 <FSEVERI\ceretta2991@lmatbi-12.fseveri.it>
2016-02-23 09:47:31 +0100

    creazione scheletro

---
+++ src/java/com/site/MainController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.site;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author FSEVERI\ceretta2991
+ */
+@Controller
+public class MainController {
+    
+    
+    
+    public MainController(){
+        
+    }
+    
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)//defaulr get metod
+    public String index(ModelMap map/*, @RequestParam(value = "id",required=false) String id*/){
+        
+        return "home";
+    }
+   
+    
+}
---
+++ web/WEB-INF/jsp/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : 23-feb-2016, 9.46.48
+    Author     : FSEVERI\ceretta2991
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
