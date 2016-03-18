b614ecb6959486c24ce5497523debda75edb4687
FSEVERI\loreggian3064 <FSEVERI\loreggian3064@lmatbi-07.fseveri.it>
2016-03-18 13:05:37 +0100

    Configurazione Spring MVC

---
+++ DipreMuseum/src/java/com/dipremuseum/MainController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dipremuseum;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author FSEVERI\loreggian3064
+ */
+@Controller
+public class MainController {
+    
+    public MainController(){
+    }
+    
+    @RequestMapping(value="/")
+    public String index(ModelMap map){
+        map.put("titolo", "Dipr&egrave Museum");
+        //List<Evento> eventi = db.getEventi();
+        //map.put("eventi", eventi);
+        return "paginaPrincipale";
+    }
+    
+}
