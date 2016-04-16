64811fe05b1fdab11bf120ab5c491b8e5a0947a4
FSEVERI\depaula2995 <FSEVERI\depaula2995@linf1-27.fseveri.it>
2016-04-16 11:13:49 +0200

    Merge origin/master
    Conflicts:
    	DipreMuseum/src/java/com/dipremuseum/MainController.java

--- DipreMuseum/src/java/com/dipremuseum/AdminController.java
+++ DipreMuseum/src/java/com/dipremuseum/AdminController.java
@@ -15,7 +15,7 @@ import models.Visita;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import po.ManageDatabase;
+import hibernate.ManageDatabase;
 
 /**
  *
